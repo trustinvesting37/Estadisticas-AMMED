50e3552499c8b4a97794d22b5065b522d4e505f1
Samuel <samuelsantana3330@gmail.com>
2022-05-26 13:47:27 -0400

    subida4

---
+++ Antecedentes/assets/js/codigo.js
@@ -0,0 +1,123 @@
+//---------------------------------------         PRIMERA GRAFICA         ------------------------------------------
+
+let ctx = document.getElementById("myChart");
+let myChart = new Chart(ctx,{
+    type: "bar",
+    data:{
+        labels:['ALERGIA A MEDICAMENTOS','DIABETES MELLITUS','ASMA', 'HTA', 'NINGUNO'],
+        datasets: [{
+            label: '',
+            data:[5,1,12,1,83],
+            backgroundColor:[
+                'rgba(103,58,183,.8)',
+                'rgba(233,30,99,.8)'
+            ]
+        }]
+    },
+    options:{
+        responsive: true,
+        indexAxis: 'y',
+        scales:{
+            yAxes:[{
+                ticks:{
+                    beginAtZero:true
+                }
+            }]
+        },
+        plugins:{
+            legend:{
+                labels:{
+                    boxWidth: 0
+                }
+            }
+        }
+    }
+});
+
+
+//---------------------------------------         Quinta GRAFICA         ------------------------------------------
+
+let ctx5 = document.getElementById("myChart5").getContext("2d");
+let myChart5 = new Chart(ctx5, {
+    type: "bar",
+    data:{
+        labels:[
+            'NINGUNO',
+            'CAFÉ',
+            'OH',
+            'TABAQUICO',
+            'VAPER',
+
+        ],
+        datasets: [{
+            label:'',
+            data:[51,46,10,2,3],
+            backgroundColor:[
+                'rgba(103,58,183,.8)',
+                'rgba(233,30,99,.8)'
+            ]
+        }]
+    },
+    options:{
+        indexAxis: 'y',
+        scales:{
+            yAxes:[{
+                ticks:{
+                    beginAtZero:true
+                }
+            }]
+        },
+        plugins:{
+            legend:{
+                labels:{
+                    boxWidth: 0
+                }
+            }
+        }
+    }
+})
+
+
+let ctx2 = document.getElementById("myChart2").getContext("2d");
+let myChart2 = new Chart(ctx2,{
+    type: 'bar',
+    data: {
+        labels:['Genero'],
+        datasets: [{
+            label: 'Masculino',
+            data: [37],
+            backgroundColor: [
+                'rgba(103,58,183,.8)',
+                
+            ]
+          
+            },
+            {
+                label:'Femenino',
+                data: [63],
+            backgroundColor: [
+                'rgba(233,30,99,.8)'
+            ]
+          
+
+            }
+        ]
+    },
+    options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          
+        }
+        
+    },
+
+
+})
---
+++ Antecedentes/total-contagiados/js/cod12.js
@@ -0,0 +1,40 @@
+function getTask2(){
+    let conjuntoRecu = JSON.parse(localStorage.getItem('recuperaciones'));
+    let apendar = $("#mostrarRecuperacion");
+    apendar.empty();
+
+    for (let i = 0; i < conjuntoRecu.length; i++) {
+        let nombre = conjuntoRecu[i].nombre;
+        let apellido = conjuntoRecu[i].apellido;
+        let cedula = conjuntoRecu[i].cedula;
+        let genero = conjuntoRecu[i].genero;
+        let edad = conjuntoRecu[i].edad;
+        let año = conjuntoRecu[i].año;
+        let estado = conjuntoRecu[i].estado;
+        let tratamiento = conjuntoRecu[i].tratamiento;
+        let conteo = i + 1;
+
+        if(año==6 && genero == "Femenino"){
+
+            apendar.append(`
+                    <tr>
+                        <td>`+conteo+`</td>
+                        <td>`+nombre+`</td>
+                        <td>`+apellido+`</td>
+                        <td>`+edad+`</td>
+                        <td>`+cedula+`</td>
+                        <td>`+estado+`</td>
+                        <td>`+tratamiento+`</td>
+                       
+                    </tr>
+                       
+            `
+            );
+        }
+        
+    };
+    
+
+}
+
+getTask2();
